b2f151d652f7bd92e8b54bd07f276be58791450c
Mina Ramsis <44512246+MinaRamsisASD1993@users.noreply.github.com>
2018-11-04 00:01:48 +0200

    Update appEs6.js

--- appEs6.js
+++ appEs6.js
@@ -1,142 +1,123 @@
-class Book {
-    constructor(title, author, isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    };
-}
+//Init variables
+const form = document.getElementById("task-form");
+const newTaskInput = document.querySelector("#task");
+const filterTaskInput = document.getElementById("filter");
+const ulList = document.querySelector("ul.collection");
+const clearTasksButton = document.querySelector(".clear-tasks");
 
-class UI {
-    constructor() {}
 
-    validateInput(book) {
-        if (book.title !== "" && book.author !== "" && book.isbn !== "") {
-            this.addToBookList(book);
-            this.showMSG("success", `Book added`);
-            this.addToLocalStorage(book);
+//form EventListener
+form.addEventListener('submit', formEventListener);
 
-        } else {
-            this.showMSG("error", `Please fill in the fields`);
-        }
+function formEventListener(e) {
+    newTaskInputValue = newTaskInput.value;
+    //Create an li element from scratch
+    if (newTaskInputValue !== "") {
+        let li = document.createElement("li");
+        li.classList = "collection-item";
+        li.innerHTML = `${newTaskInputValue}<a href="#" class="delete-item secondary-content"><i class="fa fa-remove"></i></a>`;
+        ulList.appendChild(li);
+
+        //LOCAL STORAGE STUFF
+        storeTaskInLocalStorage(newTaskInputValue);
     }
-    addToBookList(book) {
-        const row = document.createElement("tr");
-
-        row.innerHTML = `<td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="delete">X</a></td>`;
-
-        const tableBody = document.getElementById('tableBody');
-        tableBody.appendChild(row);
-
-    }
-    addToLocalStorage(book) {
-        //I'm going to add here to LS
-        const ls = new LocalStorage();
-        ls.addBookToLS(book);
-    }
-    showMSG(type, msg) {
-        const successMsg = document.querySelector('.success');
-        const errorMsg = document.querySelector('.error');
-
-        if (type === "success") {
-            successMsg.style.display = "block";
-            successMsg.innerHTML = msg;
-        } else {
-            errorMsg.style.display = "block";
-            errorMsg.innerHTML = msg;
-        }
-        setTimeout(function () {
-            successMsg.style.display = "none";
-            errorMsg.style.display = "none";
-        }, 2000);
-    }
-    clearFields() {
-        document.getElementById('bookTitle').value = "";
-        document.getElementById('author').value = "";
-        document.getElementById('isbn').value = "";
-    }
-    deleteRow(eTarget) {
-        eTarget.parentElement.parentElement.remove(); //hitting the tr element wrapping it 
-        this.showMSG("success", "Book removed!");
-        const tRow = eTarget.parentElement.parentElement;
-        const bookISBN = tRow.children[2].textContent;
-        //NOW remove book from LS
-        lsInstance.removeBookFromLS(bookISBN);
-
-    }
-
-
-}
-
-//Local Storage Class
-class LocalStorage {
-    constructor() {}
-
-    getBooksFromLS() {
-        let books;
-        if (localStorage.getItem('books') === null) {
-            books = [];
-        } else {
-            books = JSON.parse(localStorage.getItem('books'));
-        }
-        return books;
-    }
-
-    addBookToLS(book) {
-        let books = this.getBooksFromLS();
-        books.push(book);
-        //Send this books array again to LS 
-        localStorage.setItem('books', JSON.stringify(books));
-    }
-
-    onPageLoadShowLS() {
-        const ui = new UI();
-        let books = this.getBooksFromLS();
-        for (let i = 0; i < books.length; i++) {
-            // console.log(books[i]);
-            ui.addToBookList(books[i]);
-        }
-    }
-
-    removeBookFromLS(bookISBN) {
-
-        let books = this.getBooksFromLS();
-        for (let i = 0; i < books.length; i++) {
-            if (books[i].isbn === bookISBN) {
-                console.log("Delete book");
-                books.splice(i, 1);
-            }
-        }
-        //Return it back to Local Storge
-        localStorage.setItem('books', JSON.stringify(books));
-    }
-
-}
-
-
-// //on page load show Local Storage Data
-const lsInstance = new LocalStorage();
-document.addEventListener('DOMContentLoaded', lsInstance.onPageLoadShowLS());
-
-const form = document.forms[0];
-form.addEventListener('submit', addToBookList);
-//UI class validates input fields first and then shows a message which disappears after 2 seconds
-const ui = new UI();
-
-function addToBookList(e) {
-    const title = document.getElementById('bookTitle');
-    const author = document.getElementById('author');
-    const isbn = document.getElementById('isbn');
-    const book = new Book(title.value, author.value, isbn.value);
-    ui.validateInput(book);
-    //Now we clear input fields
-    ui.clearFields();
     e.preventDefault();
 }
-document.body.addEventListener('click', function (e) {
-    if (e.target.className === "delete") {
-        ui.deleteRow(e.target);
+
+function storeTaskInLocalStorage(newTaskInputValue) {
+    let tasks;
+    //Nothing in the local storage
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } //If there is an array in the Local Storage 
+    else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
     }
-});
+    tasks.push(newTaskInputValue);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+//once the document is loaded bring the local storage data
+document.addEventListener('DOMContentLoaded', getTasks);
+
+function getTasks() {
+    let tasks;
+    //Nothing in the local storage
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } //If there is an array in the Local Storage 
+    else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+    for (let i = 0; i < tasks.length; i++) {
+        let li = document.createElement("li");
+        li.classList = "collection-item";
+        li.innerHTML = `${tasks[i]}<a href="#" class="delete-item secondary-content"><i class="fa fa-remove"></i></a>`;
+        ulList.appendChild(li);
+    }
+}
+
+//ul collection event listener.. as a parent for listening 
+//to one of children (EVENT DELEGATION) .. ""if it's the target""
+
+document.addEventListener('click', deleteListItem);
+
+function deleteListItem(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+
+        if (confirm("Are You Sure?")) {
+            e.target.parentElement.parentElement.remove();
+            //Remove from LS
+            removeFromLS(e.target.parentElement.parentElement.textContent);
+        }
+
+    }
+
+}
+
+function removeFromLS(target) {
+    let tasks;
+    //Nothing in the local storage
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } //If there is an array in the Local Storage 
+    else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i] === target) {
+            tasks.splice(i, 1); //Remove this element from array
+        }
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+//ClearTasks EventLisetner
+
+clearTasksButton.addEventListener('click', clearTasks);
+
+function clearTasks(e) {
+    ulList.innerHTML = '';
+    clearLS();
+}
+
+function clearLS() {
+    localStorage.clear();
+}
+
+//filterTaskInput EventListener
+filterTaskInput.addEventListener('keyup', filterTask);
+
+function filterTask(e) {
+    let filterInput = e.target.value.toLowerCase();
+    const listItem = document.querySelectorAll("li");
+    for (let i = 0; i < listItem.length; i++) {
+        if (listItem[i].textContent.toLowerCase().includes(filterInput)) {
+            listItem[i].style.display = 'block';
+        } else {
+            listItem[i].style.display = 'none';
+        }
+    }
+
+    e.preventDefault();
+}
